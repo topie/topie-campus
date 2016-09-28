238339880014aed5d0c16f11cf03f64554052e0e
chenJz1012 <597160667@qq.com>
2016-09-28 15:21:18 +0800

    fix index.html

--- src/main/webapp/static/front/application/js/index.js
+++ src/main/webapp/static/front/application/js/index.js
@@ -5,7 +5,7 @@
 (function ($, window, document, undefined) {
     initIndex();
     $(document).ready(function () {
-        initMenu();
+        initVerticalMenu();
     });
     var requestMapping = {
         "/api/front/index": "index"
@@ -16,7 +16,31 @@
         page: function (title) {
             App.content.empty();
             App.title(title);
-            var content = $('<div class="panel-body" style="display: none" id="profile"></div>');
+            var content = $(
+                '<div class="row">' +
+                '<div class="col-md-6">' +
+                '<div class="panel panel-default" style="border-radius:0">' +
+                '<div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>个人中心</div>' +
+                '<div class="panel-body" style="display: none" id="profile"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6">' +
+                '<div class="row">' +
+                '<div class="col-md-12">' +
+                '<div class="panel panel-default" style="border-radius:0">' +
+                '<div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>通知公告</div>' +
+                '<div class="panel-body"  id="notice"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-12">' +
+                '<div class="panel panel-default" style="border-radius:0">' +
+                '<div class="panel-heading"><i class="fa fa-bar-chart-o fa-fw"></i>留言板</div>' +
+                '<div class="panel-body"  id="message"></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
             App.content.append(content);
             App.index.initEvents();
         }
@@ -41,7 +65,6 @@
                         if (data == null) {
                             return;
                         }
-
                         var temp = "";
                         if (role == 4) {
                             temp = "./tmpl/profile-teacher.html";
@@ -65,6 +88,35 @@
                         alert(result.message);
                     }
                 }
+            }
+        );
+        $("#message").load("./tmpl/message.html?t=" + new Date().getTime(),
+            function () {
+                var that = $(this);
+                var source = $(this).html();
+                that.empty();
+                $.ajax(
+                    {
+                        type: 'GET',
+                        url: App.href + "/api/front/message/send",
+                        dataType: "json",
+                        beforeSend: function (request) {
+                            request.setRequestHeader("X-Auth-Token", App.token);
+                        },
+                        success: function (result) {
+                            if (result.code === 200) {
+                                var data = result.data;
+                                var render = template.compile(source);
+                                var html = render({
+                                    "list": data.list
+                                });
+                                that.html(html).show();
+                            } else {
+                                alert(result.message);
+                            }
+                        }
+                    }
+                );
             }
         );
     };
@@ -131,6 +183,23 @@
         return ele;
     }
 
+    function secondVerticalMenu(ele, menus, subMenus) {
+        if (subMenus.length > 0) {
+            ele += "<ul class='dropdown-menu animated flipInX'>";
+            $.each(subMenus, function (i, m) {
+                ele += ('<li data-level="sub">'
+                + '<a data-url="' + m.url
+                + '" data-title="' + m.name
+                + '" href="javascript:void(0);"><i class="' + (m.icon == null ? "glyphicon glyphicon-list" : m.icon) + '"></i> '
+                + m.name
+                + '</a>');
+                ele += '</li>';
+            });
+            ele += "</ul>";
+        }
+        return ele;
+    }
+
     function thirdMenu(ele, subMenus) {
         if (subMenus.length > 0) {
             ele += "<ul class='nav nav-third-level collapse'>";
@@ -148,7 +217,7 @@
         return ele;
     }
 
-    function initMenu() {
+    function initSideMenu() {
         $.ajax(
             {
                 type: 'GET',
@@ -191,6 +260,69 @@
                                             var title = $(this).attr("data-title");
                                             App[f].page(title);
                                             $(this).parent().parent().removeClass("collapse").addClass("in");
+                                            window.history.pushState({}, 0, 'http://' + window.location.host + App.projectName + '/static/front/index.html#!' + url);
+                                        });
+                                    }
+                                }
+                            );
+                        refreshHref();
+                    } else if (result.code === 401) {
+                        alert("token失效,请登录!");
+                        window.location.href = './login.html';
+                    }
+                },
+                error: function (err) {
+                }
+            }
+        );
+    };
+
+    function initVerticalMenu() {
+        $.ajax(
+            {
+                type: 'GET',
+                url: App.href + "/api/frontCommon/current",
+                contentType: "application/json",
+                dataType: "json",
+                beforeSend: function (request) {
+                    request.setRequestHeader("X-Auth-Token", App.token);
+                },
+                success: function (result) {
+                    if (result.code === 200) {
+                        var menus = result.data;
+                        var topMenus = getTopMenu(menus);
+                        $.each(topMenus, function (i, m) {
+                            if (m.pId == 0) {
+                                var subMenus = getSubMenu(menus, m.id);
+                                var dropDown = "";
+                                if (subMenus.length > 0) {
+                                    dropDown = 'class="dropdown"'
+                                }
+                                var ele =
+                                    '<li ' + dropDown + ' data-level="top">'
+                                    + '<a data-url="' + m.url
+                                    + '" data-title="' + m.name
+                                    + '" href="javascript:void(0);"> '
+                                    + m.name
+                                    + '</a>';
+                                if (subMenus.length > 0) {
+                                    ele = secondVerticalMenu(ele, menus, subMenus);
+                                }
+                                ele += '</li>';
+                                var li = $(ele);
+                                $("#vertical-menu").append(li);
+                            }
+                        });
+                        $("#vertical-menu").find("li[class!=dropdown] > a")
+                            .each(function () {
+                                    var url = $(this).attr("data-url");
+                                    var f = App.requestMapping[url];
+                                    if (f != undefined) {
+                                        $(this).on("click", function () {
+                                            var title = $(this).attr("data-title");
+                                            App[f].page(title);
+                                            $(this).parent().siblings("li").removeClass("active");
+                                            $(this).parent().addClass("active");
                                             window.history.pushState({}, 0, 'http://' + window.location.host + App.projectName + '/static/front/index.html#!' + url);
                                         });
                                     }
