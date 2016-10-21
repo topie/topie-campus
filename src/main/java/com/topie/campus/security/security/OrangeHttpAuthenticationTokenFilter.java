2a6a3a770b21908587efc1276a0814ba8c690f7f
chenJz1012 <597160667@qq.com>
2016-10-21 11:45:57 +0800

    fix

--- src/main/java/com/topie/campus/security/security/OrangeHttpAuthenticationTokenFilter.java
+++ src/main/java/com/topie/campus/security/security/OrangeHttpAuthenticationTokenFilter.java
@@ -48,9 +48,10 @@ public class OrangeHttpAuthenticationTokenFilter extends UsernamePasswordAuthent
             authToken = request.getParameter(PARAM_TOKEN);
         }
         String username = this.tokenUtils.getUsernameFromToken(authToken);
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (username != null && !this.tokenUtils.isTokenExpired(authToken)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userCache.getUserFromCache(username);
-            if (userDetails != null && !this.tokenUtils.isTokenExpired(authToken)) {
+            if (userDetails != null) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 //authentication.setDetails(new OrangeHttpAuthenticationDetails(httpRequest));
